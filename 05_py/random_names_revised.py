72aa669780c593ba5ec34860126150c29fdf89a3
Raymond Yeung <43830708+xVany@users.noreply.github.com>
2021-09-27 20:23:57 -0400

    Update random_names_revised.py

--- 05_py/random_names_revised.py
+++ 05_py/random_names_revised.py
@@ -1,38 +1,40 @@
-# Kevin Cao, Raymond Yeung, Yaying Liang Li 
+# Kevin Cao, Raymond Yeung, Yaying Liang Li
 # Software Development
-# K02 -- Python Random Name Generator - Amalgamation
+# K05 -- Python Random Name Generator - Amalgamation
 # 2021-09-24
-
+# ----------------------------------------------------------------------------
+# Pow-Wow Summary
+# Discovery: Adding to a text file is way easier than adding to a list
+# Question:  How much do we have to comment our code?
+#            How much does runtime/efficiency matter?
+# Comments:  Combining code is pretty fun
 
 import random
 
 
-def fill_list(file, list):
-    with open(file, 'r') as file:
-        for line in file:
-            list.append(line.rstrip('/n'))
+names = {
+    'pd1': ["Owen Yaggy", "Haotian Gan", "Ishraq Mahid", "Ivan Lam",
+            "Michelle Lo", "Christopher Liu", "Ivan Mijacika", "Lucas Lee",
+            "Josephine Lee", "Rayat Roy", "Emma Buller", "William Chen",
+            "Rachel Xiao", "Andrew Juang", "Yuqing Wu", "Renggeng Zheng",
+            "Annabel Zhang", "Alejandro Alonso", "Deven Maheshwari",
+            "Oscar Wang", "Tami Takada", "Yusuf Elsharawy", "Ella Krechmer",
+            "Tomas Acuna", "Tina Nguyen", "Theo Fahey", "Sadid Ethun"],
+    'pd2': ["Patrick Ging", "Wenhao Dong", "Raymond Yeung", "Kevin Cao",
+            "Michael Borczuk", "Alif Abdullah", "Justin Zou", "Andy Lin",
+            "Shadman Rakib", "David Chong", "Daniel Sooknanan",
+            "Cameron Nelson", "Austin Ngan", "Yaying Liang Li", "Eric Guo",
+            "Noakai Aronesty", "Roshani Shrestha", "Yoonah Chang", "Qina Liu",
+            "Han Zhang", "Joshua Kloepfer"]
+}
 
+# Find length of each list
+pd1_len = len(names['pd1'])
+pd2_len = len(names['pd2'])
 
-if __name__ == '__main__':
-    # Create list for each period
-    pd1 = []
-    pd2 = []
-
-    # Fill each list from text file
-    fill_list('pd1.txt', pd1)
-    fill_list('pd2.txt', pd2)
-
-    # Find length of each list
-    pd1_len = len(pd1)
-    pd2_len = len(pd2)
-
-    # Test to see if either list is empty
-    if pd1_len < 1 or pd2_len < 1:
-        print("Lists are empty")
-
-    # Choose a random person
-    num = random.randint(0, pd1_len + pd2_len - 1)
-    if num < pd1_len:
-        print("period 1: " + pd1[num])
-    else:
-        print("period 2: " + pd2[num - pd1_len])
+# Choose a random person
+num = random.randint(0, pd1_len + pd2_len - 1)
+if num < pd1_len:
+    print("period 1: " + names['pd1'][num])
+else:
+    print("period 2: " + names['pd2'][num - pd1_len])
